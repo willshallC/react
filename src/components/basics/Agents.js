1df363d658106f798672397beccc81dcf084d8e4
willshallC <netcreaters@gmail.com>
2023-03-20 09:39:07 +0530

    Content added to First Component

--- src/components/basics/Agents.js
+++ src/components/basics/Agents.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import "./Agents.css"
 const Agents = () =>{
-    return(<div></div>);
+    return(<div>
+        <section className="agents-section">
+            <div className="agents-container">
+                <div className="agents-info-container">
+                    <div className="agents-no">1</div>
+                    <h3 className="agents-category">Duelist</h3>
+                </div>
+                <h1 className="agents-name">Jett</h1>
+                <div className="agents-img">
+                    <img src="./images/jett.jpg" alt="agent"/>
+                </div>
+            </div>
+        </section>
+    </div>);
 }
 
 export default Agents;
