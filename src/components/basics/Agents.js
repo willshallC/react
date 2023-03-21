a19b4acd85d4fe892cc8f1a549017e09086a1299
willshallC <netcreaterss@gmail.com>
2023-03-21 09:10:03 +0530

    CSS Changes and Added Logos for agents category

--- src/components/basics/Agents.js
+++ src/components/basics/Agents.js
@@ -6,9 +6,10 @@ const Agents = () =>{
             <div className="agents-container">
                 <div className="agents-info-container">
                     <div className="agents-no">1</div>
-                    <h3 className="agents-category">Duelist</h3>
+                    <h3 className="agents-category"><img className="agents-logo" src="./images/duelist.png"/> Duelist</h3>
                 </div>
                 <h1 className="agents-name">Jett</h1>
+                <p className="agents-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
                 <div className="agents-img">
                     <img src="./images/jett.jpg" alt="agent"/>
                 </div>
