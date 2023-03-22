65a4f019895185b1e6b1cc2463c2469dab24adc8
willshallC <netcreaterss@gmail.com>
2023-03-22 09:16:50 +0530

    Added Nav-bar

--- src/components/basics/Agents.js
+++ src/components/basics/Agents.js
@@ -7,6 +7,13 @@ const Agents = () =>{
 
     const [rad, setRad] = useState(Radianites);
     return(<div>
+        <div className="nav-bar">
+            <button className="btn">Duelists</button>
+            <button className="btn">Controllers</button>
+            <button className="btn">Sentinels</button>
+            <button className="btn">Initiators</button>
+            <button className="btn">Maps</button>
+        </div>
         <AgentsCards rad={rad} />
     </div>);
 }
